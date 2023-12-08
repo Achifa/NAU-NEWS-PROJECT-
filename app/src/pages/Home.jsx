e7c4cf6bc8d75e71c1043f289301f9bad85d4290
Fabian <95060643+Achifa@users.noreply.github.com>
2023-12-08 21:36:55 +0100

    fabian commit

---
+++ app/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header";
+import "../styles/xx-large.css";
+
+export default function Home() {
+    return(
+        <>
+            <Header />
+        </>
+    )
+}
